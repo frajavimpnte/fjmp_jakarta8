5f61f823b91fbb5d2e422723dfe16ad6d9d538cc
frajavimopu <frajavimopu@gmail.com>
2024-06-29 16:36:31 -0600

    Add Json Processing

---
+++ JKEE8ch03JsonProcessing/src/main/java/fjmp/jkee8ch03jsonprocessing/User.java
@@ -0,0 +1,43 @@
+package fjmp.jkee8ch03jsonprocessing;
+
+/**
+ *
+ * @author FJMP <frajavimopu@gmail.com>
+ */
+public class User {
+    private String name;
+    private String email;
+    private Integer[] profiles;
+
+    public User(String name, String email, Integer[] profiles) {
+        this.name = name;
+        this.email = email;
+        this.profiles = profiles;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer[] getProfiles() {
+        return profiles;
+    }
+
+    public void setProfiles(Integer[] profiles) {
+        this.profiles = profiles;
+    }
+    
+    
+}
